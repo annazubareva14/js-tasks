d8cfa694954ef29f6d8a6397d1b5008ffc4f3d1c
annazubareva14 <75557547+annazubareva14@users.noreply.github.com>
2021-04-15 18:10:35 +0300

    Add files via upload

--- js-tasks.js
+++ js-tasks.js
@@ -9,6 +9,28 @@ function isEven(num) {
 
 console.log (mixedArray.filter(isEven));
 
+////// с воркшопа
+
+const mixedArray = [3, 13, 74, 14, 66, 15, 22, 4];
+
+const isEven = num => {
+    return num % 2 === 0;
+}
+
+const filterArray = ( arrayToFilter, filterFn ) => {
+    const filteredArray = [];
+
+  arrayToFilter.forEach(num => {
+    if (filterFn(num)) {
+      filteredArray.push(num);
+    }
+  })
+
+  return filteredArray;
+}
+
+console.log(filterArray(mixedArray, isEven));
+
 ///// 3
 const path = "/users/download/index.html";
 console.log(path.endsWith('html'));
@@ -20,6 +42,20 @@ if (path.indexOf('html') !== -1) {
 } else {
     console.log('false');
 }
+
+
+///////// 3 с воркшопа
+
+const path = "/users/download/index.html"
+
+const isHtml = path => {
+  const requiredExt = ".html";
+  const pathExt = path.slice(-5);
+
+  return pathExt === requiredExt
+}
+
+console.log(isHtml(path));
 
 ////// 1 черновик
 
@@ -47,4 +83,54 @@ let getVowels = string.match(vowels);
 
 console.log(getVowels);
 
-//////
+////// 1 с воркшопа
+const string = "Привет! Как дела?";
+
+const vowels = ["у", "е", "ы", "а", "о", "э", "ё", "я", "и"];
+
+const getVowels = stringToFilter => {
+  let extractedVowels = "";
+
+  for (let i = 0; i < stringToFilter.length; i++) {
+    const currentLetter = stringToFilter[i].toLowerCase();
+
+    if (vowels.includes(currentLetter)) {
+      extractedVowels += currentLetter;
+    }
+  }
+
+  return extractedVowels;
+}
+
+console.log(getVowels(string));
+
+
+//////////с воркшопа
+
+const workers = [
+    { name: "John", salary: 500 },
+    { name: "Mike", salary: 1300 },
+    { name: "Linda", salary: 1500 },
+  ];
+  
+  const getWorthyWorkers = (workersArr) => {
+    const worthyWorkers = [];
+  
+    // for (let i = 0; i < workersArr.length; i++) {
+    //   const currentWorker = workersArr[i];
+  
+    //   if (currentWorker.salary > 1000) {
+    //     worthyWorkers.push(currentWorker.name);
+    //   }
+    // }
+    
+    workersArr.forEach(currentWorker => {
+      if (currentWorker.salary > 1000) {
+        worthyWorkers.push(currentWorker.name)
+      }
+    })
+  
+    return worthyWorkers;
+  };
+  
+  console.log(getWorthyWorkers(workers));
