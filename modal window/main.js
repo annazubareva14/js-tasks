a8f65b901498b042bdafb53d257baf592ce8bfb6
annazubareva14 <75557547+annazubareva14@users.noreply.github.com>
2021-04-18 09:43:00 +0300

    Add files via upload

--- modal window/main.js
+++ modal window/main.js
@@ -1,16 +1,15 @@
 const btn = document.querySelector('#btn');
 
 const overlay = document.querySelector('#overlay');
-
-
+const wrapper = document.querySelector('#wrapper');
 
 btn.addEventListener('click', function() {
     overlay.style.display = "block";
 
     const modal = document.createElement('div');
     modal.classList.add('modal-wrapper');
-    modal.style.opacity = '1';
-    overlay.appendChild(modal);
+
+    wrapper.appendChild(modal);
 
     const modalContent = document.createElement('div');
     modalContent.classList.add('modal__content');
@@ -22,11 +21,13 @@ btn.addEventListener('click', function() {
 
     closeModal.addEventListener('click', function() {
         overlay.style.display = "none";
+        wrapper.removeChild(modal);
     });
 
     overlay.addEventListener('click', e => {
         if (e.target === overlay) {
             overlay.style.display = "none";
+            wrapper.removeChild(modal);
         };  
     });
 
